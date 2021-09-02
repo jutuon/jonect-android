38f64b8fbd829e0b1934fb90e8a5baca4c2fbbb5
Juuso Tuononen <jutuon@outlook.com>
2021-09-02 20:30:52 +0300

    Add user interface

--- app/src/main/java/com/example/jonect/MainActivity.kt
+++ app/src/main/java/com/example/jonect/MainActivity.kt
@@ -6,10 +6,30 @@ package com.example.jonect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var address: EditText
+    private lateinit var status: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_connect)
+        button.setOnClickListener {
+            this.handleConnectButtonOnClick()
+        }
+
+        this.address = findViewById(R.id.edit_text_server_address)
+        this.status = findViewById(R.id.text_view_status)
+    }
+
+    private fun handleConnectButtonOnClick() {
+        val address = this.address.text.toString()
+        println(address)
+        this.status.text = address
     }
 }
